0c7201b05a478873fc269e546f6b60e155812a49
Adebola-Christianah <oguntoyeadebola21@gmail.com>
2024-09-25 17:44:24 +0100

    included render setup

--- client/src/authContext/apiCalls.js
+++ client/src/authContext/apiCalls.js
@@ -4,7 +4,7 @@ import { loginFailure, loginStart, loginSuccess } from "./AuthActions";
 export const login = async (user, dispatch) => {
   dispatch(loginStart());
   try {
-    const res = await axios.post("https://movie-app-api-two.vercel.app/auth/login", user);
+    const res = await axios.post("https://movie-app-api-two.vercel.app/login", user);
     dispatch(loginSuccess(res.data));
   } catch (err) {
     dispatch(loginFailure());
